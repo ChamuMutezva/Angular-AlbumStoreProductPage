9036587d68605c50a996b1b67b1a7f892243dd2f
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2019-10-22 21:53:31 +0200

    initial setup complete

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
+  //selector: 'app-product-description-selector',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
